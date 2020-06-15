b0e59f003d530c326e253b65c9106b8e4d8df283
guitaruser <65398433+guitaruser@users.noreply.github.com>
2020-06-15 17:36:26 +0530

    Get simple system information using python

---
+++ system_info.py
@@ -0,0 +1,8 @@
+# =====@load_thecode =====#
+# getting system info
+import platform
+# no need to pip install
+print("System :", platform.system())
+print("Node :", platform.node())
+print("Platform :", platform.platform())
+print("Processor :", platform.processor())

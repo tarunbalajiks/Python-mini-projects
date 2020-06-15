8cc26f4f5c546d15d4fbe3f6200a2468a44ff83b
guitaruser <65398433+guitaruser@users.noreply.github.com>
2020-06-15 17:39:21 +0530

    play music using this simple python snippet
    Give Filename only if File is present in same directory as the program. Else give directory with (\\) to break sequence

---
+++ playing_music.py
@@ -0,0 +1,9 @@
+# ===== load_thecode =====#
+import playsound
+
+# pip install playsound in command prompt
+# play music on machine using python
+
+filename = "enter music file/directory"
+
+playsound.playsound(filename)

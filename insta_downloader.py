1f239b32a9e7d75ca54aac7ff7e094b8e704e6df
guitaruser <65398433+guitaruser@users.noreply.github.com>
2020-06-23 14:12:58 +0530

    download any instagram post using simple python code
    Dm load_thecode if error() == True

---
+++ insta_downloader.py
@@ -0,0 +1,8 @@
+        # ===== load_thecode =====#
+import webbrowser
+
+# pip install webbrowser in command propt
+post_url = input("[+] Enter instagram url :")
+
+download_url = 'savefrom.net/' + post_url
+webbrowser.open_new_tab(download_url)
